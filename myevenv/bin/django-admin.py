c8e91fdfec0816f9be65542220cb2aab33caddc1
Yoo Young-Joo <yyj9301@gmail.com>
2019-05-28 23:51:51 +0900

    second commit of blog project

---
+++ myevenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/osx/Documents/ SYU Like Lion/19.05.24(Thurs)_Blog/myevenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
